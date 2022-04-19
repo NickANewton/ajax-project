a6589bf6d4a2ec341d1047d937621308bf2e8718
Nick Newton <nickanewton@gmail.com>
2022-04-19 05:45:52 +0000

    Task List 7-10: Access API and console log 6 results.

--- js/main.js
+++ js/main.js
@@ -8,5 +8,15 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   if (event.target === $searchForm) {
     data.search = $searchBar.value;
+    getAnimeByName(data.search);
   }
+}
+
+function getAnimeByName(search) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=' + search + '&limit=6');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+  });
+  xhr.send();
 }
