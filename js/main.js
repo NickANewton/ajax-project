d1199a111931e012c03b04490438b305dd37b865
Nick Newton <nickanewton@gmail.com>
2022-04-20 20:19:15 +0000

    Task List 7 in progress.

--- js/main.js
+++ js/main.js
@@ -31,3 +31,33 @@ function getAnimeByName(search) {
   });
   xhr.send();
 }
+
+// function searchResults(results) {
+//   var $liSearch = document.createElement('li');
+//   var $imgSearch = document.createElement('img');
+//   var $divSearch = document.createElement('div');
+//   var $h4Search = document.createElement('h4');
+//   var $p1Search = document.createElement('p');
+//   var $p2Search = document.createElement('p');
+//   var $span1Search = document.createElement('span');
+//   var $p3Search = document.createElement('p');
+//   var $span2Search = document.createElement('span');
+
+//   $liSearch.classList.add('col-half', 'desktop-display-flex', 'mb-13', 'li-styles- all', 'font-size-12');
+//   $imgSearch.classList.add('col-half', 'desktop-margin-0-10', 'border-radius-5');
+//   $divSearch.classList.add('col-half');
+//   $h4Search.classList.add('font-work-sans', 'font-size 16');
+//   $p1Search.classList.add('mt-8');
+//   $p2Search.classList.add('mt-12');
+//   $span1Search.classList.add('font-work-sans', 'font-size-16');
+//   $span2Search.classList.add('font-work-sans', 'font-size-16');
+
+//   $liSearch.appendChild($imgSearch);
+//   $liSearch.appendChild($divSearch);
+//   $divSearch.appendChild($h4Search);
+//   $divSearch.appendChild($p1Search);
+//   $divSearch.appendChild($p2Search);
+//   $p2Search.appendChild($span1Search);
+//   $divSearch.appendChild($p3Search);
+//   $p3Search.appendChild($span2Search);
+// }
